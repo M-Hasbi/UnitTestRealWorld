e1c7bd8f973da30eb84bf8b1e1e25c528e83b9d2
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-30 23:18:16 +0100

    add : CRUD methods into Repository.cs and DI implementation into Program.cs as a Services

--- UnitTestRealWorld.Web/Program.cs
+++ UnitTestRealWorld.Web/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using UnitTestRealWorld.Web.Models;
+using UnitTestRealWorld.Web.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddDbContext<UnitTestDBContext>(options =>
 {
---
+++ UnitTestRealWorld.Web/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitTestRealWorld.Web.Models;
+
+namespace UnitTestRealWorld.Web.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly UnitTestDBContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public Repository(UnitTestDBContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+
+            // _dbSet.Update(entity);
+
+            _context.SaveChanges();
+        }
+    }
+}
