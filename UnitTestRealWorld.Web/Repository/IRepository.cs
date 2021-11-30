015dd580a26c1bf2db3b0b4ec8082281d41e6ee8
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-30 22:56:15 +0100

    create : IRepository.cs

---
+++ UnitTestRealWorld.Web/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace UnitTestRealWorld.Web.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);    
+    }
+}
