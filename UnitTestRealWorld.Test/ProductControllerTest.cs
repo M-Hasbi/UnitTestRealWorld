9071343fa40d29ec02d2226d964a4584c5e4394b
M-Hasbi <kaynakhasbi1@gmail.com>
2021-12-04 10:26:52 +0100

    create : UnitTestRealWorld.Test solution create : ProductControllerTest.cs install : Moq nugget package into ProductController.cs

---
+++ UnitTestRealWorld.Test/ProductControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestRealWorld.Web.Controllers;
+using UnitTestRealWorld.Web.Models;
+using UnitTestRealWorld.Web.Repository;
+
+namespace UnitTestRealWorld.Test
+{
+    public class ProductControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+
+        private readonly ProductsController _controller;
+
+        private List<Product> _products;
+
+
+        public ProductControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+
+            _controller = new ProductsController(_mockRepo.Object);
+
+            _products = new List<Product>()
+            {
+              new Product() { Id = 1, Name = "Pencil", Price = 12, Color = "Red", Stock=100 },
+              new Product() { Id = 1, Name = "Book", Price = 15, Color = "Blue", Stock=100 }
+            };
+
+
+        }
+    }
+}
