3c9d4a9efeae1bc9fd9741a0ab67e9e050e4a6c9
M-Hasbi <kaynakhasbi1@gmail.com>
2021-12-04 10:33:23 +0100

    create : Index_ActionExecutes_ReturnView

--- UnitTestRealWorld.Test/ProductControllerTest.cs
+++ UnitTestRealWorld.Test/ProductControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using UnitTestRealWorld.Web.Controllers;
 using UnitTestRealWorld.Web.Models;
 using UnitTestRealWorld.Web.Repository;
+using Xunit;
 
 namespace UnitTestRealWorld.Test
 {
@@ -30,8 +32,13 @@ namespace UnitTestRealWorld.Test
               new Product() { Id = 1, Name = "Pencil", Price = 12, Color = "Red", Stock=100 },
               new Product() { Id = 1, Name = "Book", Price = 15, Color = "Blue", Stock=100 }
             };
+        }
+        [Fact]
+        public async void Index_ActionExecutes_ReturnView()
+        {
+            var result = await _controller.Index();
 
-
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
